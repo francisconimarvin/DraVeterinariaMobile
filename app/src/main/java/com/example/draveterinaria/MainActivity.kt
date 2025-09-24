0a18e08ade24d575f71d339eb15139b02bc51a27
sevelez <se.velez@duocuc.cl>
2025-09-23 21:09:13 -0300

    Pantalla HomeScreen con estructura Scaffold/ Yo sí pude jajajajajaja

--- app/src/main/java/com/example/draveterinaria/MainActivity.kt
+++ app/src/main/java/com/example/draveterinaria/MainActivity.kt
@@ -12,24 +12,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.draveterinaria.ui.theme.DraVeterinariaTheme
-
+import com.example.draveterinaria.ui.theme.HomeScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+
             DraVeterinariaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+                HomeScreen()
             }
         }
     }
 }
-
+/*
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -38,10 +34,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DraVeterinariaTheme {
         Greeting("Android")
     }
-}
+}*/

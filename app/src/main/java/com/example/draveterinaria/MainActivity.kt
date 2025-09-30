5ef7bf28e9f9f25d0d9c83d9cb3cc0639ed4443e
CETECOM <CETECOM@AVAWSV01LC1007.duoc.acad>
2025-09-30 20:36:18 -0300

    Solucion Error de Main

--- app/src/main/java/com/example/draveterinaria/MainActivity.kt
+++ app/src/main/java/com/example/draveterinaria/MainActivity.kt
@@ -4,25 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.draveterinaria.ui.theme.DraVeterinariaTheme
-<<<<<<< HEAD
 import com.example.draveterinaria.ui.screens.HomeScreen
-=======
-import com.example.draveterinaria.ui.theme.HomeScreen
+import com.example.draveterinaria.ui.screens.HomeScreen2
 import com.example.draveterinaria.ui.screens.HomeScreenCompacta
->>>>>>> 8bcdc5a4e8469ff081cb920b475f2174f2139dc3
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
             DraVeterinariaTheme {
-<<<<<<< HEAD
 
                 HomeScreen2()
-=======
                 HomeScreen()
                 HomeScreenCompacta()
->>>>>>> 8bcdc5a4e8469ff081cb920b475f2174f2139dc3
             }
         }
     }
